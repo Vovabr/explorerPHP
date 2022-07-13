b9b8b507eec8aeec00d6f881443192d3b651ea20
Vladimir <brattt7@gmail.com>
2022-07-13 23:21:20 +0300

    Fixed bugs

---
+++ login.php
@@ -0,0 +1,7 @@
+<form method="post">
+    <div class="explorer__options">
+        <input name="login" type="text" placeholder="admin">
+        <input name="password" type="password" placeholder="123ZXC456">
+        <button>Login</button>
+    </div>    
+</form>  
---
+++ logout.php
@@ -0,0 +1,5 @@
+<form method = "post">
+    <div class="explorer__options">
+        <button name = "logout">Logout</button>
+    </div>
+</form>
